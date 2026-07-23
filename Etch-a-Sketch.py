ba0a9c59f12bdada6a61837097b1b204a8741f67
shailaja-ps <shettarshailaja19@gmail.com>
2026-07-23 23:29:03 +0530

    Add files via upload
    A Python-based Etch-a-Sketch application built using the Turtle graphics module. The program allows users to draw interactively using keyboard controls, with options to move, rotate, and clear the canvas.

---
+++ Etch-a-Sketch.py
@@ -0,0 +1,26 @@
+from turtle import Screen,Turtle
+
+tim=Turtle()
+screen=Screen()
+
+def move_forward():
+    tim.forward(10)
+def move_backward():
+    tim.backward(10)
+def turn_left():
+    new_heading=tim.heading()+10
+    tim.setheading(new_heading)
+def turn_right():
+    new_heading=tim.heading()-10
+    tim.setheading(new_heading)
+
+
+
+
+screen.listen()
+#screen.onkey(key='space',fun=move_forward)
+screen.onkey(move_forward,'w')
+screen.onkey(move_backward,'s')
+screen.onkey(turn_left,'a')
+screen.onkey(turn_right,'d')
+screen.exitonclick()
